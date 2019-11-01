6e99c3835e94062ea0fc048e3c428724a9d28545
Fukkatsumi <fukkatsumi@mail.ua>
2019-11-02 00:01:26 +0200

    BAD hello world

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {MatButtonModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {HttpService} from './http.service';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     MatButtonModule,
 
     AppRoutingModule
@@ -15,7 +18,6 @@ import {AppComponent} from './app.component';
   declarations: [
     AppComponent
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
