22086ef4630a29746e358c9a4efb7401e5206d71
Fukkatsumi <fukkatsumi@mail.ua>
2019-11-29 02:54:12 +0200

    Apartments class added.

---
+++ src/app/component/apartments.ts
@@ -0,0 +1,10 @@
+import {ApartmentsClass} from './apartments-class';
+
+export class Apartments {
+  id: number;
+  roomNumber: number;
+  photoUrl: string;
+  description: string;
+  status: string;
+  apartmentsClass: ApartmentsClass;
+}
