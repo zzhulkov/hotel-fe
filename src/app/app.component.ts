0355152be7985ae5b45a5dba4492f583ec2c6c91
EugeneKondratiev <albrus10@mail.ru>
2019-12-16 01:14:07 +0200

    Fix my branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Hello} from './hello';
-import {ApartmentClass} from './apartmentClass';
+import {Component, OnInit} from '@angular/core';
+import {Unsubscribable} from './component/Unsubscribable';
+import { Location } from '@angular/common';
 import {HttpService} from './http.service';
-import {Subscription} from 'rxjs';
 
-const URL = 'http://localhost:8080';
+
+
 
 @Component({
   selector: 'app-root',
@@ -14,41 +13,21 @@ const URL = 'http://localhost:8080';
   providers: [HttpService]
 })
 
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent extends Unsubscribable implements OnInit {
 
-  constructor(private http: HttpClient) {
-  }
+  private userPaths: Array<string> = ['/manager/apartments', '/manager/booking',
+    '/manager/services', '/manager/staff', '/manager/tasks'];
+  condition: boolean;
 
-  title = 'hotel-fe';
-  hello: Hello;
-  apartmentClasses: ApartmentClass[];
-  selectedApartmentClass: ApartmentClass;
-  private subscription: Subscription;
-
-  onSelect(apartmentClass: ApartmentClass): void {
-    this.selectedApartmentClass = apartmentClass;
+  constructor(private location: Location) {
+    super();
   }
 
   ngOnInit() {
-    this.subscription = this.http.get(URL + '/hello').subscribe((data: Hello) => {
-      console.log(data);
-      this.hello = data;
-     });
-    this.http.get(URL + '/apartments').subscribe((data: ApartmentClass[]) => {
-      console.log(data);
-      this.apartmentClasses = data;
-    });
-  }
-
-  ngOnClick() {
-    this.subscription = this.http.get(URL + '/hello').subscribe((data: Hello) => {
-      console.log(data);
-      this.hello = data;
-     });
-  }
-
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+    if (this.userPaths.includes(this.location.path())) {
+      this.condition = false;
+    } else {
+      this.condition = true;
+    }
   }
 }
-
