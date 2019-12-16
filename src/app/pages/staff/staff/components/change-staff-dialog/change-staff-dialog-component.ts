2316e16174e7251d9bd95f366929dd925463980c
EugeneKondratiev <albrus10@mail.ru>
2019-12-16 16:24:56 +0200

    Added validation on change-staff-dialog-component

--- src/app/pages/staff/staff/components/change-staff-dialog/change-staff-dialog-component.ts
+++ src/app/pages/staff/staff/components/change-staff-dialog/change-staff-dialog-component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 /**
  * @title Dialog with header, scrollable content and actions
@@ -9,7 +10,23 @@ import {Component} from '@angular/core';
   styleUrls: ['../../../styles/change-dialog.css'],
   templateUrl: './change-staff-dialog.html',
 })
-export class ChangeStaffDialogComponent {}
+export class ChangeStaffDialogComponent {
+  changeStaffFrom = new FormGroup({
+    firstName: new FormControl('', Validators.pattern('^([A-Z]){1}([a-z]+)$')),
+    lastName: new FormControl('', Validators.pattern('^([A-Z]){1}([a-z]+)$')),
+    email: new FormControl('', Validators.pattern('^[a-zA-Z0-9.!#$%&’*+=?^_`{|}~-]+\\@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-]+$')),
+    phone: new FormControl('', Validators.pattern('(^\\+{1}\\d{7,10}$)|(^\\d{7,10}$)')),
+    login: new FormControl('', Validators.pattern('^([a-zA-Z])\\S+$')),
+    isActive: new FormControl(''),
+    speciality: new FormControl('', Validators.required)
+  });
+
+  onSubmit() {
+    if (this.changeStaffFrom.valid) {
+      console.log(this.changeStaffFrom.value);
+    }
+  }
+}
 
 
 
