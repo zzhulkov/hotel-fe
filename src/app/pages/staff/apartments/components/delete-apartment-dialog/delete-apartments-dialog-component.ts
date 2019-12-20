c1e53178c40b0c1bd6bbb20adf4b7e45d523cff5
Fukkatsumi <fukkatsumi@mail.ua>
2019-12-20 05:21:21 +0200

    Created delete request.

---
+++ src/app/pages/staff/apartments/components/delete-apartment-dialog/delete-apartments-dialog-component.ts
@@ -0,0 +1,46 @@
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Apartments} from '../../../../../component/apartments';
+import {HttpClient} from '@angular/common/http';
+import {ApartmentsClass} from '../../../../../component/apartments-class';
+
+/**
+ * @title Dialog with header, scrollable content and actions
+ */
+
+const URL = 'http://localhost:8099';
+
+@Component({
+  selector: 'app-delete-apartments-dialog',
+  styleUrls: ['../../../styles/change-dialog.css'],
+  templateUrl: './delete-apartments-dialog.html',
+})
+export class DeleteApartmentsDialogComponent {
+
+  deleteApartmentForm = new FormGroup({
+    id: new FormControl('', Validators.required)
+  });
+
+  apartment = new Apartments();
+  apartmentClass = new ApartmentsClass();
+
+  constructor(private http: HttpClient) {
+  }
+
+  onSubmit() {
+    if (this.deleteApartmentForm.valid) {
+      console.log(this.deleteApartmentForm.value);
+      this.deleteApartment();
+    }
+  }
+
+  deleteApartment() {
+    this.http.delete(URL + '/apartments/' + this.deleteApartmentForm.value.id, this.deleteApartmentForm.value).subscribe(
+      res => {
+        console.log(res);
+      });
+  }
+}
+
+
+
