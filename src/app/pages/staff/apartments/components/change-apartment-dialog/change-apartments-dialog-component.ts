213273af704048f8aa8edd251bada2b6f831c1ee
Fukkatsumi <fukkatsumi@mail.ua>
2019-12-20 05:20:51 +0200

    Created change request.

--- src/app/pages/staff/apartments/components/change-apartment-dialog/change-apartments-dialog-component.ts
+++ src/app/pages/staff/apartments/components/change-apartment-dialog/change-apartments-dialog-component.ts
@@ -1,9 +1,14 @@
 import {Component} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Apartments} from '../../../../../component/apartments';
+import {ApartmentsClass} from '../../../../../component/apartments-class';
+import {HttpClient} from '@angular/common/http';
 
 /**
  * @title Dialog with header, scrollable content and actions
  */
+
+const URL = 'http://localhost:8099';
 
 @Component({
   selector: 'app-change-apartments-dialog',
@@ -12,19 +17,50 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 export class ChangeApartmentsDialogComponent {
   profileForm = new FormGroup({
+    id: new FormControl(''),
     roomNumber: new FormControl('', Validators.required),
     photo: new FormControl(null),
     description: new FormControl(''),
     status:  new FormControl(''),
+    classId: new FormControl(''),
     className: new FormControl(''),
     numberOfRooms: new FormControl(''),
     numberOfCouchette: new FormControl('')
   });
 
+  apartment = new Apartments();
+  apartmentClass = new ApartmentsClass();
+
+  constructor(private http: HttpClient) {
+  }
+
   onSubmit() {
     if (this.profileForm.valid) {
-      console.log(this.profileForm.value);
+      this.setApartment();
+      this.createApartment();
     }
+  }
+
+  createApartment() {
+    this.http.put(URL + '/apartments/' + this.apartment.id, this.apartment).subscribe(
+    res => {
+    console.log(res);
+    this.apartment = (res as Apartments);
+    });
+  }
+
+  setApartment() {
+    this.apartmentClass.nameClass = this.profileForm.value.className;
+    this.apartmentClass.numberOfCouchette = this.profileForm.value.numberOfCouchette;
+    this.apartmentClass.numberOfRooms = this.profileForm.value.numberOfRooms;
+    this.apartmentClass.id = this.profileForm.value.classId;
+    this.apartment.apartmentClass = this.apartmentClass;
+    this.apartment.description = this.profileForm.value.description;
+    this.apartment.status = this.profileForm.value.status;
+    this.apartment.photo = this.profileForm.value.photo;
+    this.apartment.roomNumber = this.profileForm.value.roomNumber;
+    this.apartment.id = this.profileForm.value.id;
+    console.log(this.apartment);
   }
 }
 
