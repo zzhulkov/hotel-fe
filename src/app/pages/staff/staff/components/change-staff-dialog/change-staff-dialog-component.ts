e8303a3ac96068dab9db31832a7957617ae65bb0
EugeneKondratiev <albrus10@mail.ru>
2019-12-20 17:06:58 +0200

    Added validation on button 'Change'

--- src/app/pages/staff/staff/components/change-staff-dialog/change-staff-dialog-component.ts
+++ src/app/pages/staff/staff/components/change-staff-dialog/change-staff-dialog-component.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 /**
  * @title Dialog with header, scrollable content and actions
@@ -10,16 +10,31 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['../../../styles/change-dialog.css'],
   templateUrl: './change-staff-dialog.html',
 })
-export class ChangeStaffDialogComponent {
-  changeStaffFrom = new FormGroup({
-    firstName: new FormControl('', Validators.pattern('^([A-Z]){1}([a-z]+)$')),
-    lastName: new FormControl('', Validators.pattern('^([A-Z]){1}([a-z]+)$')),
-    email: new FormControl('', Validators.pattern('^[a-zA-Z0-9.!#$%&’*+=?^_`{|}~-]+\\@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-]+$')),
-    phone: new FormControl('', Validators.pattern('(^\\+{1}\\d{7,10}$)|(^\\d{7,10}$)')),
-    login: new FormControl('', Validators.pattern('^([a-zA-Z])\\S+$')),
-    isActive: new FormControl(''),
-    speciality: new FormControl('', Validators.required)
-  });
+export class ChangeStaffDialogComponent implements OnInit {
+
+  changeStaffFrom: FormGroup;
+
+  ngOnInit(): void {
+    this.changeStaffFrom = this.formBuilder.group({
+      firstName: ['', Validators.pattern('^([A-Z]){1}([a-z]+)$')],
+      lastName: ['', Validators.pattern('^([A-Z]){1}([a-z]+)$')],
+      email: ['', Validators.pattern('^[a-zA-Z0-9.!#$%&’*+=?^_`{|}~-]+\\@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-]+$')],
+      phone: ['', [Validators.pattern('(^\\+{1}\\d{7,13}$)|(^\\d{7,13}$)')]],
+      login: ['', Validators.pattern('^([a-zA-Z])\\S+$')],
+    });
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+  }
+
+  checkValid() {
+    this.changeStaffFrom.markAllAsTouched();
+    console.log('FormGroup: ', this.changeStaffFrom.valid);
+  }
+
+  isSubmitDisabled(): boolean {
+    return !this.changeStaffFrom.valid ;
+  }
 
   onSubmit() {
     if (this.changeStaffFrom.valid) {
