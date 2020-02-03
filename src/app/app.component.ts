35c61d02e5ea86298308be1d886c5910ddd55718
Fukkatsumi <fukkatsumi@mail.ua>
2020-02-03 22:38:56 +0200

    Condition has been simplified.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Unsubscribable} from './component/Unsubscribable';
 import {Location} from '@angular/common';
 import {HttpService} from './http.service';
-import {SelectService} from "./services/select.service";
+import {SelectService} from './services/select.service';
 
 
 @Component({
@@ -14,8 +14,6 @@ import {SelectService} from "./services/select.service";
 
 export class AppComponent extends Unsubscribable implements OnInit {
 
-  private userPaths: Array<string> = ['/manager/apartments', '/manager/booking',
-    '/manager/services', '/manager/staff', '/manager/tasks', '/manager/apartments-classes', '/manager/users'];
   condition: boolean;
 
   constructor(private location: Location, public selectService: SelectService) {
@@ -24,10 +22,6 @@ export class AppComponent extends Unsubscribable implements OnInit {
 
   ngOnInit() {
     console.log('init');
-    if (this.userPaths.includes(this.location.path())) {
-      this.condition = false;
-    } else {
-      this.condition = true;
-    }
+    this.condition = !this.location.path().match('^/manager/');
   }
 }
